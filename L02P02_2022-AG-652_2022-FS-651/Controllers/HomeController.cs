e5083498c6dddb847a7a698c88d62125553339ed
DARWIN PROYECTOS <darwin.aguirre1@catolica.edu.sv>
2025-04-03 18:26:37 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022-AG-652_2022-FS-651/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using L02P02_2022_AG_652_2022_FS_651.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2022_AG_652_2022_FS_651.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ L02P02_2022-AG-652_2022-FS-651/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022_AG_652_2022_FS_651.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
